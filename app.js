320cdc1de9e3c3ace7958e77b6fe252ece8c743a
Pranab <pranab.mitra@widespace.com>
2018-09-02 22:07:07 +0600

    Get Top News - Initial Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const API_KEY = 'b9fe8b5378ae4198be8b3b68a68be3d2';
+const contentWrapper = document.querySelector('main');
+const sourceElement = document.querySelector('#sourceSelector');
+const defaultSource = 'the-washington-post';
+
+window.addEventListener('load', async e => {
+    updateNews();
+    await updateSources();
+
+    sourceElement.value = defaultSource;
+    sourceElement.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+});
+
+async function updateSources() {
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${API_KEY}`);
+    const json = await res.json();
+
+    sourceElement.innerHTML = json.sources
+        .map(src => `<option value=${src.id}>${src.name}</option>`)
+        .join('\n');
+}
+
+async function updateNews(source = defaultSource) {
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${API_KEY}`);
+    const json = await res.json();
+
+    contentWrapper.innerHTML = json.articles.map(crateArticle).join('\n'); 
+}
+
+function crateArticle(article) {
+    return `
+        <div class="article">
+            <a href="${article.url}">
+                <h2>${article.title}</h2>
+                <img src="${article.urlToImage}" />
+                <p>${article.description}</p>
+            </a>
+        </div>
+    `;
+}
