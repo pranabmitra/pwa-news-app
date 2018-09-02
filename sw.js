e6db8d75e3ed276d47bef14ab3edce648f473dae
Pranab <pranab.mitra@widespace.com>
2018-09-02 23:59:08 +0600

    Cache first strategy implemented

---
+++ sw.js
@@ -0,0 +1,21 @@
+const staticAssets = [
+    './',
+    './style.css',
+    './app.js'
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open('news-static');
+    cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    
+    return cachedResponse || fetch(req);
+}
