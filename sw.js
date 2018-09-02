62b3a7aadcddf647f45c656e9c1d0ac3d9b286ef
Pranab <pranab.mitra@widespace.com>
2018-09-03 00:18:39 +0600

    Network first strategy implemented

--- sw.js
+++ sw.js
@@ -11,11 +11,32 @@ self.addEventListener('install', async event => {
 
 self.addEventListener('fetch', event => {
     const req = event.request;
-    event.respondWith(cacheFirst(req));
+    const url = new URL(req);
+
+    if (url.origin === location.origin) { 
+        // site's static assets
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req));
+    }
+    
 });
 
 async function cacheFirst(req) {
     const cachedResponse = await caches.match(req);
-    
+
     return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('news-dynamic');
+
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+
+        return res;
+    } catch (error) {
+        return await cache.match(req);   
+    }
 }
